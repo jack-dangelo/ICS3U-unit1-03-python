1649dfc47c016e8a3dcac7696d772f254bf07293
jack-dangelo <54954654+jack-dangelo@users.noreply.github.com>
2019-12-02 12:26:36 -0500

    Create fancy_hello_world.py

---
+++ fancy_hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Jack D'angelo
+# Created on: September 2019
+# This is the "Hello, World!" program, but with nice style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
